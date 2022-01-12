f966dc05993e1edea5802818594bb7f1e3cb5ab7
exthitikorn <rockerzeeza@gmail.com>
2022-01-12 09:20:23 +0700

    make it better

--- index.js
+++ index.js
@@ -33,12 +33,7 @@ function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
-  } else if (event.message.type == "text" || event.message.text == "Hello")
-    const payload = {
-      type: "text",
-      text: "Hello From Heroku Server"
-    };
-    return client.replyMessage(event.replyToken, payload);
+  }
 
   // create a echoing text message
   const echo = { type: 'text', text: event.message.text };
